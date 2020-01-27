e922909394dfd2f821bd1baa0d35fda610f4ae35
iranroman <iran@ccrma.stanford.edu>
2020-01-27 06:53:41 -0800

    updates tfgrfnn objects and oscillator types

--- tfgrfnn/oscillator_types.py
+++ tfgrfnn/oscillator_types.py
@@ -1,13 +1,23 @@
 import numpy as np
 import tensorflow as tf
 
-class hopfpar():
+class hopf():
 
     def __init__(self, alpha=0.0, beta1=-1.0, beta2=0.0, 
-                delta1=0.0, delta2=0.0, epsilon = 0.0):
+                    delta1=0.0, delta2=0.0, epsilon = 0.0):
     
-        self.alpha = beta2
+        self.alpha = alpha
         self.beta1 = beta1
+        self.beta2 = beta2
         self.delta1 = delta1
         self.delta2 = delta2
         self.epsilon = epsilon
+
+    def params(self):
+        params_dict = {'alpha': self.alpha, 
+                        'beta1': self.beta1, 
+                        'beta2': self.beta2, 
+                        'delta1': self.delta1, 
+                        'delta2': self.delta2, 
+                        'epsilon': self.epsilon}
+        return params_dict
--- tfgrfnn/tfgrfnn.py
+++ tfgrfnn/tfgrfnn.py
@@ -1,44 +1,67 @@
 import numpy as np
 import tensorflow as tf
 
-from oscillator_types import hopf_params
+from oscillator_types import hopf
+from connection_learning_rule_types import phase_lock 
 
-class oscillator_layer():
+class oscillators():
 
-    def __init__(self, name='', 
-                    osctype='Hopf',
-                    freqlims=(0.12, 8.0),
-                    freqspac='log',
-                    nosc=256,
-                    initconds=np.zeros((256,)),
-                    save_states=True):
+    def __init__(self, name = '', 
+                    osctype = hopf(),
+                    nosc = 256,
+                    freqlims = (0.12, 8.0),
+                    freqspace = 'log',
+                    initconds = np.zeros((256, )),
+                    save_steps = True):
 
         self.name = name
         self.osctype = osctype
-        if self.osctype == 'Hopf'
-            self.params = hopf_params()
-        self.freqspac = freqspac
+        self.params = self.osctype.params()
+        self.freqspace = freqspace
         self.freqlims = freqlims
         self.nosc = nosc
         if self.freqspac == 'log':
             self.freqs = np.logspace(np.log10(self.freqlims[0]),
-                    np.log10(self.freqlims[1]),self.nosc)
+                            np.log10(self.freqlims[1]), 
+                            self.nosc)
             if self.nosc > 1:
-                self.freqdelta = self.freqs[1]/self.freqs[0]
+                self.freqdelta = self.freqs[1] / self.freqs[0]
             else:
                 self.freqdelta = 1
         elif self.freqspac == 'lin':
             self.freqs = np.linpace(self.freqlims[0],
-                    self.freqlims[1],self.nosc)
+                            self.freqlims[1],
+                            self.nosc)
             if self.nosc > 1:
-                self.freqdelta = self.freqs[1]-self.freqs[0]
+                self.freqdelta = self.freqs[1] - self.freqs[0]
             else:
                 self.freqdelta = 1
         self.initconds = initconds
-        self.save_states = save_states
+        self.save_steps = save_steps
         self.connections = []
 
 
-class connection_matrix():
+class connection():
 
-    def __init__(self, ):
+    def __init__(self, name = '', 
+                    source = None,
+                    target = None,
+                    conn_type = None,
+                    amplitude = 1.0,
+                    range_ = 0.12,
+                    learn = True,
+                    complex_ = True,
+                    save_steps = True):
+
+        self.name = name
+        self.source = source
+        self.target = target
+        self.conn_type = conn_type 
+        self.save_steps = save_steps
+
+def connect(source = None, target = None, conn_matrix = None, conn_type = None, ):
+    
+    target.connections.append(conn_matrix) 
+
+    return target
+        
