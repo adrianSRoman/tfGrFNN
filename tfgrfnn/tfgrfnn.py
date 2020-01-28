3baafaf0eb1dfe1ec20794b98496458bd5fc1c0b
iranroman <iran@ccrma.stanford.edu>
2020-01-28 13:39:09 -0800

    initiates model class

--- tfgrfnn/tfgrfnn.py
+++ tfgrfnn/tfgrfnn.py
@@ -64,4 +64,59 @@ def connect(source = None, target = None, conn_matrix = None, conn_type = None,
     target.connections.append(conn_matrix) 
 
     return target
+       
+
+class Model():
+
+    def __init__(self, name = '',
+                    layers = None,
+                    zfun = xdot_ydot,
+                    cfun = crdot_cidot,
+                    time = tf.range(1, 0.01, dtype = tf.float64),
+                    dt = 0.01):
+
+        self.layers = layers
+        self.time = time
+        self.zfun = zfun
+        self.cfun = cfun
+        self.dt = dt
+
+    def integrate(self):
+
+        all_osc_init_conds, all_conn_init_conds = [oscillatos.initconds, [connection.initconds for connection in oscillators] 
+                                                    for oscillators in self.layers]
+
+        def scan_func(all_init_conds, time):
+
+            all_osc_init_conds, all_conn_init_conds = all_init_conds
+
+            all_osc_stepsi, all_conn_steps = [self.odeRK4(self.zfun, time, self.dt, osc_init_conds, M.layers[iosc].params)
+                                                [self.odeRK4(self.cfun, time, self.dt, conn_init_conds, M.layers[iconn].params)
+                                                    for iconn, conn_init_conds in enumerate(all_conn_init_conds[ilayer])]
+                                                for iosc, osc_init_conds in enumerate(all_osc_init_conds)]  
+            all_conn_steps = [self.odeRK4(self.cfun, time, self.dt, conn_init_conds) for conn_init_conds in all_conn_init_conds]  
+
+            return all_osc_steps, all_conn_steps
+
+        all_osc_and_conn_steps = tf.scan(scan_func, time, (all_osc_init_conds, all_conn_init_conds))
         
+        all_osc_steps, all_conn_steps = all_osc_and_conn_steps
+
+        for ilayer in len(self.layers):
+            self.layers[ilayer].savedStates = all_osc_steps[ilayer]
+            for iconn in len(self.layers[ilayer].connections):
+                self.layers[ilayer].connections[iconn].savedStates = all_conn_steps[ilayer,iconn]
+
+    def odeRK4(self, func, time, dt, state, params):
+
+        t_half_dt = tf.add(time,dt/2)
+
+        k1 = func(t, state, params)
+        k2 = func(t_half_dt, tf.add(state, tf.scalar_mul(dt,k1/2)), params)
+        k3 = func(t_half_dt, tf.add(state, tf.scalar_mul(dt,k2/2)), params)
+        k4 = func(tf.add(t, d), tf.add(state, tf.scalar_mul(dt,k3)), params)
+
+        state = state + tf.scalar_mul(dt, tf.add_n([k1, 2*k2, 2*k3, k4])/6)
+
+        return state
+
