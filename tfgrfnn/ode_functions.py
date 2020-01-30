5ce92ccf50e2459eae5e957895831b6e0612ae14
iranroman <iran@ccrma.stanford.edu>
2020-01-29 19:20:10 -0800

    updates rk4 integrator

--- tfgrfnn/ode_functions.py
+++ tfgrfnn/ode_functions.py
@@ -1,18 +1,21 @@
 import tensorflow as tf
 import numpy as np
+import tfgrfnn as tg
 
-def xdot_ydot(xt_yt, alpha, beta1, beta2, epsilon, ones, freqs, lrn_stim_connmat_params_zip):
+def xdot_ydot(t, t_idx, xt_yt, connections, alpha=None, beta1=None, beta2=None, epsilon=None, freqs= None, ones=None):
+
+    omega = tf.constant(2*np.pi, dtype=tf.float64)
 
     xt, yt = tf.split(xt_yt, 2, axis=0)
-    minusyt_plusxt = tfconcat(tf.scalar_mul(-1,yt), xt, axis=0)
+    minusyt_plusxt = tf.concat([tf.scalar_mul(-1,yt), xt], axis=0)
 
     x2tplusy2t = tf.add(tf.pow(xt, 2),
                         tf.pow(yt, 2))
-    x2tplusy2t_x2tplusy2t = tf.concat(x2tplusy2t, 
-                                x2tplusy2t, axis=0)
+    x2tplusy2t_x2tplusy2t = tf.concat([x2tplusy2t, 
+                                x2tplusy2t], axis=0)
     x2tplusy2tsquared = tf.pow(x2tplusy2t, 2)
-    x2tplusy2tsquared_x2tplusy2tsquared = tf.concat(x2tplusy2tsquared, 
-                                x2tplusy2tsquared, axis=0)
+    x2tplusy2tsquared_x2tplusy2tsquared = tf.concat([x2tplusy2tsquared, 
+                                x2tplusy2tsquared], axis=0)
 
     xtnew_ytnew = tf.add_n([
                             tf.scalar_mul(alpha, xt_yt),
@@ -25,20 +28,29 @@ def xdot_ydot(xt_yt, alpha, beta1, beta2, epsilon, ones, freqs, lrn_stim_connmat
                                     tf.scalar_mul(epsilon, x2tplusy2t_x2tplusy2t)))
                             ])
 
-    csrt_csit = tf.add_n([compute_inputs(lrn_stim_connmat_params) for lrn_stim_connmat_params in list(lrn_stim_connmat_params_zip)])
-
-    dxdt_dydt = tf.multiply(freqs, tf.add(xtnew_ytnew, csrt_csit))
+    if connections == None:
+        dxdt_dydt = tf.multiply(freqs, xtnew_ytnew)
+    else:
+        csrt_csit = tf.add_n([compute_inputs(t_idx, conn) for conn in connections])
+        dxdt_dydt = tf.multiply(freqs, tf.add(xtnew_ytnew, csrt_csit))
 
     return dxdt_dydt
 
-def compute_inputs(lrn_stim_connmat_params):
+def compute_inputs(time_index, conn):
 
-    lrn_type, srt_sit, crt_cit, params = lrn_stim_connmat_params
+    if isinstance(conn.source, tg.oscillators):
+        conn_type, srt_sit, crt_cit, params = conn.type, conn.source.state, conn.matrix, conn.params
+    else: 
+        if isinstance(time_index, int):
+            conn_type, srt_sit, crt_cit, params = conn.type, conn.source.state[time_index], conn.matrix, conn.params
+        else:
+            conn_type, srtm1_sitm1, srtp1_sitp1, crt_cit, params = conn.type, conn.source.state[int(time_index-0.5)], conn.source.state[int(time_index+0.5)], conn.matrix, conn.params
+            srt_sit = srtm1_sitm1*0.5 + srtp1_sitp1*0.5
 
     srt, sit = tf.split(srt_sit, 2, axis=0)
     crt, cit = tf.split(crt_cit, 2, axis=0)
 
-    csrt_csit = tf.switch_case(lrn_type, 
+    csrt_csit = tf.switch_case(conn_type, 
                             branch_fns={
                                         0: compute_input_1freq(crt, cit, srt, sit)
                                         }
@@ -51,7 +63,7 @@ def compute_input_1freq(cr, ci, srt, sit):
     csrt = tf.matmul(cr, srt)
     csit = tf.matmul(ci, srt)
 
-    csrt_csit = tf.concat(csrt, csit, axis=0)
+    csrt_csit = tf.concat([csrt, csit], axis=0)
 
     return csrt_csit 
 
@@ -61,11 +73,11 @@ def crdot_cidot(crt_cit, lrn_type, lambda_, mu1, mu2, kappa, xst_yst, xtt_ytt, f
 
     cr2tplusci2t = tf.add(tf.pow(crt, 2),
                         tf.pow(cit, 2))
-    cr2tplusci2t_cr2tplusci2t = tf.concat(cr2tplusci2t, 
-                                cr2tplusci2t, axis=0)
+    cr2tplusci2t_cr2tplusci2t = tf.concat([cr2tplusci2t, 
+                                cr2tplusci2t], axis=0)
     cr2tplusci2tsquared = tf.pow(cr2tplusci2t, 2)
-    cr2tplusci2tsquared_cr2tplusci2tsquared = tf.concat(cr2tplusci2tsquared, 
-                                cr2tplusci2tsquared, axis=0)
+    cr2tplusci2tsquared_cr2tplusci2tsquared = tf.concat([cr2tplusci2tsquared, 
+                                cr2tplusci2tsquared], axis=0)
 
     dcrt_dcit = tf.switch_case(lrn_type, 
                             branch_fns={
@@ -80,9 +92,9 @@ def update_connection_1freq(crt_cit, lambda_, mu1, mu2, kappa, epsilonc, ones, x
     xst, yst = tf.split(xst_yst, 2, axis=0)
     xtt, ytt = tf.split(xtt_ytt, 2, axis=0)
     
-    xss_xss = tf.concat(xss, xss, axis=0)
-    plusytt_minusxtt = tf.concat(ytt, tf.scalar_mul(-1, xtt), axis=0) 
-    yss_yss = tf.concat(yss, yss, axis=0)
+    xss_xss = tf.concat([xss, xss], axis=0)
+    plusytt_minusxtt = tf.concat([ytt, tf.scalar_mul(-1, xtt)], axis=0) 
+    yss_yss = tf.concat([yss, yss], axis=0)
 
     dcrdt_dcidt = tf.add_n(
                         tf.scalar_mul(lambda_, crt_cit),
--- tfgrfnn/tfgrfnn.py
+++ tfgrfnn/tfgrfnn.py
@@ -1,23 +1,22 @@
 import numpy as np
 import tensorflow as tf
 
-from oscillator_types import hopf
+from oscillator_types import canonical_hopf
 from connection_learning_rule_types import phase_lock 
 from ode_functions import xdot_ydot, crdot_cidot
 
 class oscillators():
 
     def __init__(self, name = '', 
-                    osctype = hopf(),
+                    osctype = canonical_hopf(),
                     nosc = 256,
                     freqlims = (0.12, 8.0),
                     freqspacing = 'log',
-                    initconds = np.zeros((256, )),
-                    save_steps = True):
+                    initconds = None,
+                    savesteps = True):
 
         self.name = name
         self.osctype = osctype
-        self.params = self.osctype.params()
         self.freqspacing = freqspacing
         self.freqlims = freqlims
         self.nosc = nosc
@@ -25,22 +24,24 @@ class oscillators():
             self.freqs = np.logspace(np.log10(self.freqlims[0]),
                             np.log10(self.freqlims[1]), 
                             self.nosc)
-            if self.nosc > 1:
-                self.freqdelta = self.freqs[1] / self.freqs[0]
-            else:
-                self.freqdelta = 1
         elif self.freqspacing == 'lin':
             self.freqs = np.linpace(self.freqlims[0],
                             self.freqlims[1],
                             self.nosc)
-            if self.nosc > 1:
-                self.freqdelta = self.freqs[1] - self.freqs[0]
-            else:
-                self.freqdelta = 1
-        self.initconds = initconds
-        self.save_steps = save_steps
-        self.connections = []
+        self.params = self.osctype.params()
+        self.params['freqs'] = tf.constant(self.freqs, dtype=tf.float64)
+        self.params['ones'] = tf.constant(1, dtype=tf.float64, shape=(self.nosc,))
+        if initconds == None:
+            self.initconds = tf.constant(0, dtype=tf.float64, shape=(self.nosc,))
+            self.currstep = tf.constant(0, dtype=tf.float64, shape=(self.nosc,))
+        else:
+            self.initconds = tf.constant(initconds)
+            self.currstep = tf.constant(initconds)
+        self.savesteps = savesteps
+        self.connections = None
 
+    def __repr__(self):
+        return "<Layer with %s %s oscillators>" % (self.nosc, self.osctype)
 
 class connection():
 
@@ -81,51 +82,73 @@ class Model():
         self.zfun = zfun
         self.cfun = cfun
         self.dt = dt
+        self.half_dt = dt/2
+        if self.zfun == xdot_ydot:
+            for layer in self.layers:
+                layer.params['ones'] = tf.concat([layer.params['ones'], layer.params['ones']], axis=0)
+                layer.params['freqs'] = tf.concat([layer.params['freqs'], layer.params['freqs']], axis=0)
+                layer.currstep = tf.concat([tf.math.real(layer.currstep), tf.math.imag(layer.currstep)], axis=0)
+        for layer in self.layers:
+            if layer.savesteps:
+                layer.allsteps = tf.constant(0, shape=(layer.currstep.shape[0], self.time.shape[0]))
 
+    @tf.function
     def integrate(self):
 
-        all_layers_initconds, all_layers_connections = [(layer.initconds, layer.connections) for layer in self.layers]
-        all_conns_initconds = [[connection.initconds for connection in layer_connections] for layer_connections in all_layers_connections]
+        def odeRK4(t_idx):
 
-        def odeRK4(all_init_conds, time):
+            t = self.time[t_idx] 
+            t_plus_half_dt = tf.add(t, self.half_dt)
+            t_plus_dt = tf.add(t, self.dt)
 
-            time_plus_half_dt = tf.add(time, self.dt/2)
+            for layer in self.layers:
+                layer.k0 = layer.currstep
+                layer.k1 = self.zfun(t, t_idx, layer.currstep, layer.connections, **layer.params)
+                layer.currstep = tf.add(layer.currstep, tf.scalar_mul(self.dt, layer.k1/2))
+                if layer.connections != None:
+                    for conn in layer.connections:
+                        conn.k0 = conn.matrix
+                        conn.k1 = self.cfun(t, conn.matrix, conn.params) 
+                        conn.matrix = tf.add(conn.matrix, tf.scalar_mul(self.dt, conn.k1/2))
 
-            all_layers_state, all_conns_state = all_init_conds
+            for layer in self.layers:
+                layer.k2 = self.zfun(t_plus_half_dt, tf.cast(t_idx, dtype=tf.float64)+0.5, layer.currstep, layer.connections, **layer.params)
+                layer.currstep = tf.add(layer.currstep, tf.scalar_mul(self.dt, layer.k2/2))
+                if layer.connections != None:
+                    for conn in layer.connections:
+                        conn.k2 = self.cfun(t_plust_half_dt, conn.matrix, conn.params) 
+                        conn.matrix = tf.add(conn.matrix, tf.scalar_mul(self.dt, conn.k2/2))
 
-            all_layers_k1 = [self.zfun(time, layer_state, M.layers[ilayer].params) for ilayer, layer_state in enumerate(all_layers_state)]
-            all_conns_k1 = [[self.cfun(time, conn_state, M.layers[ilayer].connections[iconn].params) 
-                                for iconn, conn_state in enumerate(all_conns_state[ilayer])] 
-                            for ilayer in range(len(all_layers_state))]
-            all_layers_k2 = [self.zfun(time_plus_half_dt, tf.add(layer_state, tf.scalar_mul(self.dt, layer_k1/2)), M.layers[ilayer].params) 
-                            for ilayer, (layer_state, layer_k1) in enumerate(zip(all_layers_state, all_layers_k1))]
-            all_conns_k2 = [[self.cfun(time_plus_half_dt, tf.add(conn_state, tf.scalar_mul(self.dt, conn_k1/2)), M.layers[ilayer].connections[iconn].params) 
-                                for iconn, (conn_state, conn_k1) in enumerate(zip(all_conns_state[ilayer], all_conns_k1[ilayer]))] 
-                            for ilayer in range(len(all_layers_state))]
-            all_layers_k3 = [self.zfun(time_plus_half_dt, tf.add(layer_state, tf.scalar_mul(self.dt, layer_k2/2)), M.layers[ilayer].params) 
-                            for ilayer, (layer_state, layer_k2) in enumerate(zip(all_layers_state, all_layers_k2))]
-            all_conns_k3 = [[self.cfun(time_plus_half_dt, tf.add(conn_state, tf.scalar_mul(self.dt, conn_k2/2)), M.layers[ilayer].connections[iconn].params) 
-                                for iconn, (conn_state, conn_k2) in enumerate(zip(all_conns_state[ilayer], all_conns_k2[ilayer]))] 
-                            for ilayer in range(len(all_layers_state))]
-            all_layers_k4 = [self.zfun(tf.add(time, self.dt), tf.add(layer_state, tf.scalar_mul(self.dt, layer_k3)), M.layers[ilayer].params) 
-                            for ilayer, (layer_state, layer_k3) in enumerate(zip(all_layers_state, all_layers_k3))]
-            all_conns_k4 = [[self.cfun(tf.add(time, self.dt), tf.add(conn_state, tf.scalar_mul(self.dt, conn_k3)), M.layers[ilayer].connections[iconn].params) 
-                                for iconn, (conn_state, conn_k3) in enumerate(zip(all_conns_state[ilayer], all_conns_k3[ilayer]))] 
-                            for ilayer in range(len(all_layers_state))]
-            all_layers_state = [tf.add(layer_state, tf.divide(tf.add_n([layer_k1, tf.scalar_mul(2, layer_k2), tf.scalar_mul(2, layer_k3), layer_k4]), 6))
-                                for layer_state, layer_k1, layer_k2, layer_k3, layer_k4 
-                                    in zip(all_layers_state, all_layers_k1, all_layers_k2, all_layers_k3, all_layers_k4)] 
-            all_conns_state = [[tf.add(conn_state, tf.divide(tf.add_n([conn_k1, tf.scalar_mul(2, conn_k2), tf.scalar_mul(2, conn_k3), conn_k4]), 6))
-                                    for conn_state, conn_k1, conn_k2, conn_k3, conn_k4 
-                                        in zip(all_conns_state[ilayer], all_conns_k1[ilayer], all_conns_k2[ilayer], all_conns_k3[ilayer], all_conns_k4[ilayer])]
-                                for ilayer in range(len(all_layers_state))]
+            for layer in self.layers:
+                layer.k3 = self.zfun(t_plus_half_dt, tf.cast(t_idx, dtype=tf.float64)+0.5, layer.currstep, layer.connections, **layer.params)
+                layer.currstep = tf.add(layer.currstep, tf.scalar_mul(self.dt, layer.k3))
+                if layer.connections != None:
+                    for conn in layer.connections:
+                        conn.k3 = self.cfun(t_plus_half_dt, conn.matrix, conn.params) 
+                        conn.matrix = tf.add(conn.matrix, tf.scalar_mul(self.dt, conn.k3))
+            
+            for layer in self.layers:
+                layer.k4 = self.zfun(t_plus_dt, t_idx+1, layer.currstep, layer.connections, **layer.params)
+                layer.currstep = tf.add(layer.k0, 
+                                    tf.scalar_mul(self.dt/6, tf.add_n([layer.k1, 
+                                                                        tf.scalar_mul(2, layer.k2), 
+                                                                        tf.scalar_mul(2, layer.k3), 
+                                                                        layer_k4])))
+                layer.allsteps[:,t_idx] = layer.curr_step
+                if layer.connections != None:
+                    for conn in layer.connections:
+                        conn.k4 = self.cfun(t_plus_dt, conn.matrix, conn.params) 
+                        conn.matrix = tf.add(conn.k0, 
+                                            tf.scalar_mul(self.dt/6, tf.add_n([conn.k1, 
+                                                                                tf.scalar_mul(2, conn.k2), 
+                                                                                tf.scalar_mul(2, conn.k3), 
+                                                                                conn.k4])))
 
-            return all_layers_state, all_conns_state
+            return t_idx + 1
 
-        all_layers_steps, all_conns_steps = tf.scan(odeRK4, time, (all_layers_initconds, all_conns_initconds))
-        
-        for ilayer in range(len(self.layers)):
-            self.layers[ilayer].saved_states = all_layers_steps[ilayer]
-            for iconn in range(len(self.layers[ilayer].connections)):
-                self.layers[ilayer].connections[iconn].saved_states = all_conns_steps[ilayer][iconn]
-
+        t_idx = tf.constant(0)
+        tf.while_loop(lambda t_idx :tf.less(t_idx, len(self.time)), odeRK4(t_idx), t_idx)
+        if self.zfun == xdot_ydot:
+            for layer in self.layers:
+                x, y = tf.split(layer.allsteps, 2, axis=0)
+                layer.allsteps = x + 1j*y
