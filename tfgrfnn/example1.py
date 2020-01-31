38f54d4ee3c160c6fd94fe5e3063926472972402
iranroman <iran@ccrma.stanford.edu>
2020-01-31 12:41:27 -0800

    rk4 integrator completed

--- tfgrfnn/example1.py
+++ tfgrfnn/example1.py
@@ -1,12 +1,14 @@
+import tensorflow as tf
 import tfgrfnn as tg
 from oscillator_types import canonical_hopf
 
 l1 = tg.oscillators(name='l1', osctype=canonical_hopf(alpha=1.0, beta1=-1.0, beta2=0.0, epsilon=0.0), 
-                    freqspacing='log', freqlims=(0.5, 2), nosc=2, savesteps=True)
-l2 = tg.oscillators(name='l1', osctype=canonical_hopf(alpha=1.0, beta1=-1.0, beta2=0.0, epsilon=0.0), 
-                    freqspacing='log', freqlims=(0.5, 2), nosc=2, savesteps=True)
+                    freqspacing='log', freqlims=(0.5, 2.0), nosc=501, savesteps=True)
 
-GrFNN = tg.Model(name='GrFNN', layers=[l1])
+GrFNN = tg.Model(name='GrFNN', layers=[l1], time=tf.range(10, delta = 0.01, dtype = tf.float64))
 
-GrFNN.integrate()
+GrFNN = GrFNN.integrate()
+
+print(GrFNN.layers[0].allsteps)
+
 
--- tfgrfnn/tfgrfnn.py
+++ tfgrfnn/tfgrfnn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import time
 
 from oscillator_types import canonical_hopf
 from connection_learning_rule_types import phase_lock 
@@ -82,24 +83,11 @@ class Model():
         self.cfun = cfun
         self.dt = dt
         self.half_dt = dt/2
-        if self.zfun == xdot_ydot:
-            for layer in self.layers:
-                layer.params['ones'] = tf.concat([layer.params['ones'], layer.params['ones']], axis=0)
-                layer.params['freqs'] = tf.concat([layer.params['freqs'], layer.params['freqs']], axis=0)
-                layer.initconds = tf.concat([tf.math.real(layer.initconds), tf.math.imag(layer.initconds)], axis=0)
-                layer.nosc = layer.nosc*2
-                for conn in layer.connections:
-                    conn.matrix = tf.concat([tf.math.real(conn.matrix), tf.math.imag(conn.matrix)], axis=0)
-        #for layer in self.layers:
-        #    if layer.savesteps:
-        #        layer.allsteps = []
-        self.all_layers_init_conds = [layer.initconds for layer in self.layers]
-        self.all_conns_init_conds = [[[conn.source.initconds, conn.matrix] for conn in layer.connections] for layer in self.layers]
 
     @tf.function
-    def integrate(self):
+    def odeRK4(self):
 
-        def odeRK4(all_states, t_idx):
+        def scan_fun(all_states, t_idx):
 
             t = self.time[t_idx] 
             t_plus_half_dt = tf.add(t, self.half_dt)
@@ -115,45 +103,73 @@ class Model():
                             for ilayer in range(len(all_layers_state))]
 
             all_layers_k2 = [self.zfun(t_plus_half_dt, tf.add(tf.cast(t_idx, dtype=tf.float64),0.5), 
-                                tf.add(all_layers_state[ilayer], tf.scalar_mul(self.dt, all_layers_k1[ilayer])),
+                                tf.add(all_layers_state[ilayer], tf.scalar_mul(self.half_dt, all_layers_k1[ilayer])),
                                 all_conns_state[ilayer], 
                                 self.layers[ilayer].connections, 
                                 **self.layers[ilayer].params) 
                             for ilayer in range(len(all_layers_state))]
 
             all_layers_k3 = [self.zfun(t_plus_half_dt, tf.add(tf.cast(t_idx, dtype=tf.float64),0.5), 
-                                tf.add(all_layers_state[ilayer], tf.scalar_mul(self.dt, all_layers_k2[ilayer])),
+                                tf.add(all_layers_state[ilayer], tf.scalar_mul(self.half_dt, all_layers_k2[ilayer])),
                                 all_conns_state[ilayer], 
                                 self.layers[ilayer].connections, 
                                 **self.layers[ilayer].params) 
                             for ilayer in range(len(all_layers_state))]
 
             all_layers_k4 = [self.zfun(t_plus_dt, tf.add(t_idx, 1), 
-                                tf.add(all_layers_state[ilayer], tf.scalar_mul(self.dt, all_layers_k2[ilayer])),
+                                tf.add(all_layers_state[ilayer], tf.scalar_mul(self.dt, all_layers_k3[ilayer])),
                                 all_conns_state[ilayer], 
                                 self.layers[ilayer].connections, 
                                 **self.layers[ilayer].params) 
                             for ilayer in range(len(all_layers_state))]
 
             all_layers_state = [tf.add(all_layers_state[ilayer],
-                                    tf.scalar_mul(tf.divide(self.dt, 6), tf.add_n([all_layers_k1[ilayer],
-                                                                                tf.scalar_mul(2, all_layers_k2[ilayer]),
-                                                                                tf.scalar_mul(2, all_layers_k3[ilayer]),
-                                                                                all_layers_k4[ilayer]])))
+                                    tf.scalar_mul(tf.divide(self.dt, 6), 
+                                        tf.add_n([all_layers_k1[ilayer],
+                                                    tf.scalar_mul(2, all_layers_k2[ilayer]),
+                                                    tf.scalar_mul(2, all_layers_k3[ilayer]),
+                                                    all_layers_k4[ilayer]])))
                                 for ilayer in range(len(all_layers_state))]
 
-
-            #all_conns_state = [[[tf.add(all_conns_state[ilayer][iconn][0], 1), tf.add(all_conns_state[ilayer][iconn][1],1)]
-            #                        for iconn in range(len(all_conns_state[ilayer]))] 
-            #                    for ilayer in range(len(all_layers_state))]
 
             return [all_layers_state, all_conns_state]
 
         all_states = [self.all_layers_init_conds, self.all_conns_init_conds]
-        x = tf.scan(odeRK4, tf.range(5), all_states)
-        tf.print(len(x))
-        tf.print(len(x[0]))
-        tf.print(len(x[1]))
-        tf.print(len(x[0][0]))
-        tf.print(len(x[1][0]))
-        tf.print(len(x[1][0][0][0]))
+        all_layers_steps, all_conns_steps = tf.scan(scan_fun, tf.range(len(self.time)), all_states)
+        
+        return all_layers_steps, all_conns_steps
+
+    def integrate(self):
+
+        if self.zfun == xdot_ydot:
+            for layer in self.layers:
+                layer.params['ones'] = tf.concat([layer.params['ones'], layer.params['ones']], axis=0)
+                layer.params['freqs'] = tf.concat([layer.params['freqs'], layer.params['freqs']], axis=0)
+                layer.initconds = tf.concat([tf.math.real(layer.initconds), tf.math.imag(layer.initconds)], axis=0)
+                layer.nosc = layer.nosc*2
+                for conn in layer.connections:
+                    conn.matrix = tf.concat([tf.math.real(conn.matrix), tf.math.imag(conn.matrix)], axis=0)
+        self.all_layers_init_conds = [layer.initconds for layer in self.layers]
+        self.all_conns_init_conds = [[[conn.source.initconds, conn.matrix] for conn in layer.connections] for layer in self.layers]
+
+        t = time.time()
+        all_layers_steps, all_conns_steps = self.odeRK4()
+        elapsed = time.time()-t
+        print(elapsed)
+
+        del self.all_layers_init_conds
+        del self.all_conns_init_conds
+        if self.zfun == xdot_ydot:
+            for ilayer, layer in enumerate(self.layers):
+                layer.params['ones'], _ = tf.split(layer.params['ones'], 2, axis=0)
+                layer.params['freqs'], _ = tf.split(layer.params['freqs'], 2, axis=0)
+                initconds_real, initconds_imag = tf.split(layer.initconds, 2, axis=0)
+                layer.initconds = tf.complex(initconds_real, initconds_imag)
+                layer.nosc = layer.nosc/2
+                layer_steps_real, layer_steps_imag = tf.split(tf.transpose(all_layers_steps[ilayer]), 2, axis=0)
+                layer.allsteps = tf.complex(layer_steps_real, layer_steps_imag)
+                for conn in layer.connections:
+                    conn_matrix_real, conn_matrix_imag = tf.split(conn.matrix, 2, axis=0)
+                    conn.matrix = tf.complex(conn_matrix_real, conn_matrix_imag)
+       
+        return self
