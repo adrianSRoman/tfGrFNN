8db9d1a8c0bd9018f26b00075226de935a201de5
iranroman <iran@ccrma.stanford.edu>
2020-02-06 19:42:18 -0800

    work on tf.scan RK4

--- tfgrfnn/tfgrfnn.py
+++ tfgrfnn/tfgrfnn.py
@@ -122,9 +122,10 @@ class Model():
         self.half_dt = dt/2
 
     @tf.function
-    def odeRK4(self):
+    def odeRK4(self, layers_and_connmats_state, time_dt_stims):
 
-        def scan_fn(layers_and_connmats_state, time_dt_stims):
+
+        def scan_fn(layers_and_connmats_state, time_dts_stims):
 
             layers_state, connmats_state = layers_and_connmats_state
             t, dt, stim, stim_shift = time_dt_stim
@@ -132,116 +133,112 @@ class Model():
             t_plus_half_dt = tf.add(t, dt/2)
             t_plus_dt = tf.add(t, dt)
 
+            layers_k0 = layers_state.copy()
+            connmats_k0 = connmats_state.copy()
+
             layers_k1 = [self.zfun(t, layer_state, connmats_states[ilayer], 
                             self.layers[ilayer].connections.sourcesintid, 
                             layers_state, **self.layers[ilayer].params)
                         for ilayer, layer_state in enumerate(layers_state)]
-            connmats_k1 = [[self.cfun(layers_state, connmat, 
+            connmats_k1 = [[self.cfun(t, layers_state, connmat_state, 
                                 layer_state[self.layers[ilayer].connections[iconn].learnparams['learntypeint'], 
                                 self.layer[ilayer].connections[iconn].learnparams) 
-                            for iconn, connmat in enumerate(connmats_state)] 
+                            for iconn, connmat_state in enumerate(connmats_state)] 
                         for ilayer, layer_state in enumerate(layers_state))]
+            layers_state = [tf.add(layer_k0, tf.scalar_mul(dt/2, layers_k1[ilayer])) 
+                        for ilayer, layer_k0 in enumerate(layers_k0)]
+            connmats_state = [tf.add(connmat_k0, tf.scalar_mul(dt/2, connmats_k1[ilayer][ilayer]))
+                            for iconn, connmat_k0 in enumerate(connmats_k0[ilayer])
+                        for ilayer in range(len(layers_k0))]
 
-            for layer in self.layers:
-                layer.k0 = layer.currstate
-                layer.k1 = self.zfun(t, t_idx, 
-                                layer.currstate,
-                                layer.connections,
-                                **layer.params) 
-                for conn in layer.connections:
-                    conn.matrix_k0 = conn.matrixstate
-                    conn.matrix_k1 = self.cfun(t, t_idx,
-                                            conn.source.currstate,
-                                            conn.matrixstate,
-                                            conn.target.currstate,
-                                            conn.learnparams)
-            for layer in self.layers:
-                layer.currstate = tf.add(layer.k0, tf.scalar_mul(self.half_dt, layer.k1))
-                for conn in layer.connections:
-                    conn.matrixstate = tf.add(conn.matrix_k0, tf.scalar_mul(self.half_dt, conn.matrix_k1))
+            layers_k2 = [self.zfun(t_plus_half_dt, layer_state, connmats_states[ilayer], 
+                            self.layers[ilayer].connections.sourcesintid, 
+                            layers_state, **self.layers[ilayer].params)
+                        for ilayer, layer_state in enumerate(layers_state)]
+            connmats_k2 = [[self.cfun(t_plus_half_dt, layers_state, connmat_state, 
+                                layer_state[self.layers[ilayer].connections[iconn].learnparams['learntypeint'], 
+                                self.layer[ilayer].connections[iconn].learnparams) 
+                            for iconn, connmat_state in enumerate(connmats_state)] 
+                        for ilayer, layer_state in enumerate(layers_state))]
+            layers_state = [tf.add(layer_k0, tf.scalar_mul(dt/2, layers_k2[ilayer])) 
+                        for ilayer, layer_k0 in enumerate(layers_k0)]
+            connmats_state = [tf.add(connmat_k0, tf.scalar_mul(dt/2, connmats_k2[ilayer][ilayer]))
+                            for iconn, connmat_k0 in enumerate(connmats_k0[ilayer])
+                        for ilayer in range(len(layers_k0))]
 
-            for layer in self.layers:
-                layer.k2 = self.zfun(t_plus_half_dt, tf.add(tf.cast(t_idx, dtype=tf.float64), 0.5), 
-                                layer.currstate,
-                                layer.connections,
-                                **layer.params) 
-                for conn in layer.connections:
-                    conn.matrix_k2 = self.cfun(t_plus_half_dt, tf.add(tf.cast(t_idx, dtype=tf.float64), 0.5), 
-                                            conn.source.currstate,
-                                            conn.matrixstate,
-                                            conn.target.currstate,
-                                            conn.learnparams)
-            for layer in self.layers:
-                layer.currstate = tf.add(layer.k0, tf.scalar_mul(self.half_dt, layer.k2))
-                for conn in layer.connections:
-                    conn.matrixstate = tf.add(conn.matrix_k0, tf.scalar_mul(self.half_dt, conn.matrix_k2))
+            layers_k3 = [self.zfun(t_plus_half_dt, layer_state, connmats_states[ilayer], 
+                            self.layers[ilayer].connections.sourcesintid, 
+                            layers_state, **self.layers[ilayer].params)
+                        for ilayer, layer_state in enumerate(layers_state)]
+            connmats_k3 = [[self.cfun(t_plus_half_dt, layers_state, connmat_state, 
+                                layer_state[self.layers[ilayer].connections[iconn].learnparams['learntypeint'], 
+                                self.layer[ilayer].connections[iconn].learnparams) 
+                            for iconn, connmat_state in enumerate(connmats_state)] 
+                        for ilayer, layer_state in enumerate(layers_state))]
+            layers_state = [tf.add(layer_k0, tf.scalar_mul(dt, layers_k2[ilayer])) 
+                        for ilayer, layer_k0 in enumerate(layers_k0)]
+            connmats_state = [tf.add(connmat_k0, tf.scalar_mul(dt, connmats_k2[ilayer][ilayer]))
+                            for iconn, connmat_k0 in enumerate(connmats_k0[ilayer])
+                        for ilayer in range(len(layers_k0))]
 
-            for layer in self.layers:
-                layer.k3 = self.zfun(t_plus_half_dt, tf.add(tf.cast(t_idx, dtype=tf.float64), 0.5), 
-                                layer.currstate,
-                                layer.connections,
-                                **layer.params) 
-                for conn in layer.connections:
-                    conn.matrix_k3 = self.cfun(t_plus_half_dt, tf.add(tf.cast(t_idx, dtype=tf.float64), 0.5), 
-                                            conn.source.currstate,
-                                            conn.matrixstate,
-                                            conn.target.currstate,
-                                            conn.learnparams)
-            for layer in self.layers:
-                layer.currstate = tf.add(layer.k0, layer.k3)
-                for conn in layer.connections:
-                    conn.matrixstate = tf.add(conn.matrix_k0, conn.matrix_k3)
+            layers_k4 = [self.zfun(t_plus_dt, layer_state, connmats_states[ilayer], 
+                            self.layers[ilayer].connections.sourcesintid, 
+                            layers_state, **self.layers[ilayer].params)
+                        for ilayer, layer_state in enumerate(layers_state)]
+            connmats_k4 = [[self.cfun(t_plus_dt, layers_state, connmat_state, 
+                                layer_state[self.layers[ilayer].connections[iconn].learnparams['learntypeint'], 
 
-            for layer in self.layers:
-                layer.k4 = self.zfun(t_plus_dt, tf.add(t_idx, 1), 
-                                layer.currstate,
-                                layer.connections,
-                                **layer.params) 
-                for conn in layer.connections:
-                    conn.matrix_k4 = self.cfun(t_plus_dt, tf.add(t_idx, 1), 
-                                            conn.source.currstate,
-                                            conn.matrixstate,
-                                            conn.target.currstate,
-                                            conn.learnparams)
+            layers_state = [tf.add(layer_k0, 
+                            tf.multiply(dt/6,  tf.add_n([layers_k1[ilayer],
+                                                        tf.scalar_mul(2, layers_k2[ilayer]),
+                                                        tf.scalar_mul(2, layers_k3[ilayer]),
+                                                        layers_k4[ilayer]])))
+                        for ilayer, layer_k0 in enumerate(layers_k0)]
+            connmats_state = [[tf.add(conn_k0, 
+                            tf.multiply(dt/6,  tf.add_n([connmats_k1[ilayer][iconn],
+                                                        tf.scalar_mul(2, connmats_k2[ilayer][iconn]),
+                                                        tf.scalar_mul(2, connmats_k3[ilayer][iconn]),
+                                                        connmats_k4[ilayer][iconn]])))]
+                            for iconn, conn_k0 in enumerate(connmats_k0[ilayer])
+                        for ilayer in range(len(layers_k0))]
 
-            for layer in self.layers:
-                layer.currstate = tf.add(layer.k0, 
-                                    tf.scalar_mul(tf.divide(self.dt, 6),
-                                        tf.add_n([layer.k1, 
-                                                    tf.scalar_mul(2, layer.k2),
-                                                    tf.scalar_mul(2, layer.k3),
-                                                    layer.k4]))), self
-                layer.allsteps.append(copy.copy(layer.currstate))
-                for conn in layer.connections:
-                    conn.matrixstate = tf.add(conn.matrix_k0,
-                                    tf.scalar_mul(tf.divide(self.dt, 6),
-                                        tf.add_n([conn.matrix_k1,
-                                                    tf.scalar_mul(2, conn.matrix_k2),
-                                                    tf.scalar_mul(2, conn.matrix_k3),
-                                                    conn.matrix_k4])))
-                    conn.matrixsteps.append(conn.matrixstate)
+            return [layers_state, connmats_state]
+
+        dts = self.time[1:] - self.time[:-1]
+        layers_state, connmats_state = tf.scan(scan_fn, 
+                    [self.time[:-1], dts, self.stim.values[:-1], self.stim.values[1:]], 
+                    [layers_state, connmats_state])
+
+        return layers_state, connmats_state
 
     def integrate(self):
 
         if self.zfun == xdot_ydot:
             for layer in self.layers:
                 layer.params['freqs'] = tf.concat([layer.params['freqs'], layer.params['freqs']], axis=0)
-                layer.currstate = tf.concat([tf.math.real(layer.initconds), tf.math.imag(layer.initconds)], axis=0)
+                layer.initconds = tf.concat([tf.math.real(layer.initconds), tf.math.imag(layer.initconds)], axis=0)
                 layer.nosc = layer.nosc*2
                 for conn in layer.connections:
-                    conn.matrixstate = tf.concat([tf.math.real(conn.matrixinit), tf.math.imag(conn.matrixinit)], axis=0)
+                    conn.matrixinit = tf.concat([tf.math.real(conn.matrixinit), tf.math.imag(conn.matrixinit)], axis=0)
             for stim in self.stimuli:
                 stim.values = tf.concat([tf.math.real(stim.values), tf.math.imag(stim.values)], axis=0)
-                stim.currstate = stim.values[:,0]
 
-        self.odeRK4()
+        layers_state = [layer.currstate for layer in self.layers]
+        connmats_state = [[conn.matrixstate for conn in layers.connections] for layer in layers]
+
+        layers_state, connmats_state = self.odeRK4(layers_state, connmats_state)
 
         if self.zfun == xdot_ydot:
             for stim in self.stimuli:
                 stim_values_real, stim_values_imag = tf.split(stim.values, 2, axis=0)
                 stim.values = tf.complex(stim_values_real, stim_values_imag)
             for layer in self.layers:
+                layer_initconds_real, layer_initconds_imag = tf.split(layer.initconds, 2, axis=0)
+                layer.initconds = tf.complex(layer_initconds_real, layer_initconds_imag)
                 layer.params['freqs'], _ = tf.split(layer.params['freqs'], 2, axis=0)
                 layer.nosc = layer.nosc/2
+                for conn in layer.connections:
+                    conn_matrixinit_real, conn_matrixinit_imag = tf.split(conn.matrixinit, 2, axis=0)
+                    conn.matrixinit = tf.complex(conn_matrixinit_real, conn_matrixinit_imag)
        
         return self
