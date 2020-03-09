65e9a7a225a240ae3cacfb26ec5820b95c80ab0f
iranroman <iran@ccrma.stanford.edu>
2020-03-09 11:51:43 -0700

    updates example 1

--- tfgrfnn/example1.py
+++ tfgrfnn/example1.py
@@ -3,15 +3,16 @@ import tensorflow as tf
 import tfgrfnn as tg
 import matplotlib.pyplot as plt
 
+
 # simulation duration values
-dur = 50.0
+dur = 50
 dt = 0.025
 fs = int(1/dt)
 time = tf.range(dur, delta=dt, dtype=tf.float32)
 # GrFNN network values and parameters
 N = 201
-freqs = tf.constant(np.logspace(np.log10(0.5),np.log10(2.0),N),dtype=tf.float32)
-initconds = tf.constant(0.1+1j*0.0, dtype=tf.complex64, shape=(N,))
+freqs = tf.constant(np.logspace(np.log10(0.5), np.log10(2.0),N), dtype=tf.float32)
+initconds = tf.constant(0.01+1j*0.0, dtype=tf.complex64, shape=(N,))
 params_dict = {'alpha':tf.constant(0.0, dtype=tf.float32), 
              'beta1':tf.constant(-1.0, dtype=tf.float32),
              'beta2':tf.constant(-1.0, dtype=tf.float32),
@@ -37,15 +38,34 @@ s1 = tg.stimulus(name = 's1',
                 values = stim_values,
                 fs = fs)
 # connect stimulus and oscillators
-l1 = tg.connect(source=s1, target=l1, matrixinit=tf.constant(1.0+1j*0.0, dtype=tf.complex64, shape=(1,N)))
+l1 = tg.connect(source=s1, target=l1, 
+                matrixinit=tf.constant(1.0+1j*0.0, 
+                                        dtype=tf.complex64, shape=(1,N)))
 # define the model
 GrFNN = tg.Model(name='GrFNN', layers=[l1], stim=s1)
 # integrate 
 GrFNN = GrFNN.integrate()
+
+
 # plot results
+nxticks = 6
+nyticks = 10
+plt.subplot(1,2,1)
 plt.semilogx(GrFNN.layers[0].params['freqs'],np.abs(GrFNN.layers[0].states[0,:,-1]))
 plt.ylim([0, 1.3*np.max(np.abs(GrFNN.layers[0].states[0,:,-1]))])
 plt.grid()
 plt.xlabel('Oscillator natural frequency (Hz)')
-plt.ylabel('Manitude')
+plt.ylabel('Amplitude')
+plt.title('Amplitude of oscillators (t = '+str(dur)+'s)')
+ax = plt.subplot(1,2,2)
+plt.imshow(np.abs(GrFNN.layers[0].states[0]), aspect = 'auto')
+ax.set_xticklabels([str(round(float(label),2)) for label in GrFNN.time.numpy()\
+        [np.round(np.linspace(0,len(GrFNN.time)-1,nxticks)).astype(int)]])
+ax.set_yticklabels([str(round(float(label),2)) for label in GrFNN.layers[0].params['freqs'].numpy()[::-1]\
+        [np.round(np.linspace(0,len(GrFNN.layers[0].params['freqs'])-1,nyticks)).astype(int)]])
+plt.xlabel('Time (s)')
+plt.ylabel('Oscillator natural frequency (Hz)')
+plt.colorbar().set_label('Amplitude')
+plt.title('Amplitude of oscillators over time')
+plt.tight_layout()
 plt.savefig('example1.png')
---
+++ tfgrfnn/ode_functions.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+import numpy as np
+import tfgrfnn as tg
+
+def xdot_ydot(t, xt_yt, connmats_state, connections, sources_state, alpha=None, beta1=None, beta2=None, epsilon=None, freqs= None):
+
+    omega = tf.constant(2*np.pi, dtype=tf.float32)
+
+    xt, yt = tf.split(xt_yt, 2, axis=1)
+    minusyt_plusxt = tf.concat([tf.multiply(-1.0,yt), xt], axis=1)
+
+    x2tplusy2t = tf.add(tf.pow(xt, 2),
+                        tf.pow(yt, 2))
+    x2tplusy2t_x2tplusy2t = tf.concat([x2tplusy2t, 
+                                x2tplusy2t], axis=1)
+    x2tplusy2tsquared_x2tplusy2tsquared = tf.pow(x2tplusy2t_x2tplusy2t, 2)
+
+    xtnew_ytnew = tf.add_n([tf.multiply(alpha, xt_yt),
+                            tf.multiply(omega, minusyt_plusxt),
+                            tf.multiply(beta1, tf.multiply(xt_yt, x2tplusy2t_x2tplusy2t)),
+                            tf.divide(
+                                tf.multiply(tf.multiply(epsilon, beta2), 
+                                    tf.multiply(xt_yt, x2tplusy2tsquared_x2tplusy2tsquared)),
+                                tf.subtract(tf.constant(1.0, dtype=tf.float32), 
+                                    tf.multiply(epsilon, x2tplusy2t_x2tplusy2t)))])
+
+    csrt_csit = tf.add_n([tf.multiply(connections[iconn].learnparams['weight'], compute_input(connmat_state, 
+                            sources_state[connections[iconn].sourceintid], 
+                            connections[iconn].learnparams['learntypeint']))
+                        for iconn, connmat_state in enumerate(connmats_state)]) if connmats_state else 0
+            
+    dxdt_dydt = tf.multiply(freqs, tf.add(xtnew_ytnew, csrt_csit))
+    
+    return dxdt_dydt
+
+
+def compute_input(connmat_state, source_state, learntypeint):
+
+    def compute_input_nolearning(srt_sit=source_state, crt_cit=connmat_state):
+
+        srt, sit = tf.split(srt_sit, 2, axis=1)
+        crt, cit = tf.split(crt_cit, 2, axis=1)
+        csrt = tf.matmul(srt, crt) - tf.matmul(sit, cit)
+        csit = tf.matmul(sit, crt) + tf.matmul(srt, cit)
+
+        csrt_csit = tf.concat([csrt, csit], axis=1)
+
+        return csrt_csit 
+
+    def compute_input_1freq(srt_sit=source_state, crt_cit=connmat_state):
+
+        srt, sit = tf.split(srt_sit, 2, axis=1)
+        crt, cit = tf.split(crt_cit, 2, axis=1)
+        csrt = tf.matmul(srt, crt) - tf.matmul(sit, cit)
+        csit = tf.matmul(sit, crt) + tf.matmul(srt, cit)
+
+        csrt_csit = tf.concat([csrt, csit], axis=1)
+
+        return csrt_csit 
+
+    csrt_csit = tf.switch_case(learntypeint,
+                                branch_fns={0: compute_input_nolearning,
+                                            1: compute_input_1freq})
+
+    return csrt_csit
+
+def crdot_cidot(t, xst_yst, crt_cit, xtt_ytt, learnparams):
+
+    def nolearning(t=t, xst_yst=xst_yst, crt_cit=crt_cit, xtt_ytt=xtt_ytt, learnparams=learnparams):
+        
+        return tf.constant(0, dtype=tf.float32, shape=crt_cit.shape)
+    '''
+    def learn_1freq(t=t, xst_yst=xst_yst, crt_cit=crt_cit, xtt_ytt=xtt_ytt, learnparams=learnparams):
+        
+        lambda_ = learnparams['lambda_']
+        mu1 = learnparams['mu1']
+        mu2 = learnparams['mu2']
+        kappa = learnparams['kappa']
+        epsilon = learnparams['epsilon']
+        freqss = tf.expand_dims(learnparams['freqss'], 0)
+        freqst = tf.expand_dims(learnparams['freqst'], -1)
+
+        xst, yst = tf.split(xst_yst, 2, axis=0)
+        crt, cit = tf.split(crt_cit, 2, axis=0)
+        xtt, ytt = tf.split(xtt_ytt, 2, axis=0)
+
+        cr2tplusci2t = tf.add(tf.pow(crt, 2),
+                            tf.pow(cit, 2))
+        cr2tplusci2t_cr2tplusci2t = tf.concat([cr2tplusci2t, 
+                                    cr2tplusci2t], axis=0)
+        cr2tplusci2tsquared_cr2tplusci2tsquared = tf.pow(cr2tplusci2t_cr2tplusci2t, 2)
+
+        dcrdt_dcidt = tf.add_n([tf.multiply(lambda_, crt_cit),
+                            tf.multiply(mu1, tf.multiply(crt_cit, cr2tplusci2t_cr2tplusci2t)),
+                            tf.divide(tf.multiply(tf.multiply(epsilon, mu2), 
+                                        tf.multiply(crt_cit, cr2tplusci2tsquared_cr2tplusci2tsquared)),
+                                    tf.subtract(tf.constant(1.0, dtype=tf.float32), 
+                                        tf.multiply(epsilon, cr2tplusci2t_cr2tplusci2t))),
+                            tf.multiply(kappa, tf.concat([tf.add(tf.matmul(tf.expand_dims(xtt,-1), 
+                                                                            tf.expand_dims(xst,0)), 
+                                                                tf.matmul(tf.expand_dims(ytt,-1), 
+                                                                            tf.expand_dims(yst,0))),
+                                                            tf.add(tf.matmul(tf.expand_dims(ytt,-1), 
+                                                                            tf.expand_dims(xst,0)),
+                                                                tf.matmul(tf.expand_dims(-xtt,-1), 
+                                                                            tf.expand_dims(yst,0)))], axis=0))])
+
+        fmat = tf.add(tf.tile(freqst, tf.shape(freqss)), 
+                tf.tile(freqss, tf.shape(freqst)))
+        dcrdt_dcidt = tf.multiply(tf.divide(tf.concat([fmat, fmat], axis=0), 2), dcrdt_dcidt)
+
+        return dcrdt_dcidt
+    '''
+
+    learntype = learnparams['learntypeint']
+
+    dcrdt_dcidt = tf.switch_case(learntype,
+                                    branch_fns={0: nolearning})
+                                                #1: learn_1freq})
+
+    return dcrdt_dcidt
